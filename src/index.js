c75f9472d1ab4dcdd8b74000feeda7c8f19a4f74
Tim Dorr <timdorr@timdorr.com>
2016-02-25 02:12:34 -0500

    Add Ava and Babel. Scaffold some things.

---
+++ src/index.js
@@ -0,0 +1,67 @@
+class WebpackConf {
+
+  // Setup methods
+
+  constructor(config) {
+    if (!!config && typeof config === 'object')
+      this.loadConfig(config)
+    else
+      this._setDefault()
+  }
+
+  _setDefault() {
+    this.entry = {
+      main: 'index.js'
+    }
+    this.output = {
+      filename: 'bundle.js'
+    }
+    this.loaders = []
+    this.plugins = []
+  }
+
+  loadConfig(config) {
+    // reset the config to the defaults before loading
+    this._setDefault()
+
+    this.entry = config.entry || this.entry
+    this.output = config.output || this.output
+    this.loaders = config.module && config.module.loaders || this.loaders
+    this.plugins = config.plugins || this.plugins
+  }
+
+  // Entry
+
+  setEntry(entry) {
+    this.entry = entry
+  }
+
+  addEntry(name, entry) {
+    if (typeof this.entry === 'string')
+      this.entry = { main: this.entry }
+
+    if (Array.isArray(this.entry))
+      this.entry = { main: this.entry }
+
+    this.entry[name] = entry
+  }
+
+  // Output
+
+
+  // Export
+
+  toConfig() {
+    return {
+      entry: this.entry
+    }
+  }
+
+  toJSON() {
+    return JSON.stringify({
+      entry: this.entry
+    })
+  }
+}
+
+export default WebpackConf
---
+++ test/index.spec.js
@@ -0,0 +1,8 @@
+import test from 'ava'
+import WebpackConf from '../src/index'
+
+test('it exports a class', t => {
+  const webpack = new WebpackConf()
+  t.is(typeof webpack, 'object')
+  t.ok(webpack instanceof WebpackConf)
+})
